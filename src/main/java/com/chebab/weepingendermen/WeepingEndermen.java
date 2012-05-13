420b527612ac99f8130dd0154306c4b11fbd114e
Patrik Lembke <blambi@chebab.com>
2012-05-13 23:04:07 +0200

    A lot simpler algoritm

--- src/main/java/com/chebab/weepingendermen/WeepingEndermen.java
+++ src/main/java/com/chebab/weepingendermen/WeepingEndermen.java
@@ -8,6 +8,7 @@ import java.util.Random;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.Server;
 import org.bukkit.World;
+import org.bukkit.Chunk;
 import org.bukkit.configuration.file.FileConfiguration;
 
 import org.bukkit.event.Event;
@@ -53,10 +54,14 @@ public class WeepingEndermen extends JavaPlugin implements Listener
     @EventHandler
     public void onEntityDamageByEntity( EntityDamageByEntityEvent event ) {
         Player victim;
+        World world;
         // Something hit something check if the target is a player
         if( event.getEntityType() == EntityType.PLAYER && event.getDamager().getType() == EntityType.ENDERMAN ) {
+            // Check that we are allowed to effect this world
+            world = event.getEntity().getWorld();
+
             // TODO: Add a random thing here
-            if( rnd.nextInt(5) == 0 || true ) {
+            if( worlds.indexOf( world.getName() ) != -1 && rnd.nextInt(5) == 0 ) {
                 victim = (Player)event.getEntity();
                 randomSafeTeleport( victim );
                 victim.sendMessage("*Poof*");
@@ -68,79 +73,62 @@ public class WeepingEndermen extends JavaPlugin implements Listener
         Location current = victim.getLocation();
         Location proposed = current.clone();
         double x, z, y;
-        boolean has_floor = false;
-        Block block;
+        //boolean has_floor = false;
+        boolean has_safespot = false;
+        Block block, block_under;
         int safe_cnt = 0;
+        World world = victim.getWorld();
+        Chunk chunk;
 
         // Find a safe spot
-        while( safe_cnt < 3 && !has_floor ) {
-            x = (rnd.nextDouble() * 200) - 100;
-            z = (rnd.nextDouble() * 200) - 100;
-            y = (double)-1.0;
-            has_floor = false;
-            // victim.sendMessage("Checking if " + x + "x" + z + " is any good");
-            proposed.add( x, y, z );
+        while( !has_safespot ) {
+            if( rnd.nextBoolean() )
+                x = (rnd.nextDouble() * 200);
+            else
+                x = -(rnd.nextDouble() * 200);
 
-            for( double x2 = proposed.getY(); x2<=254.0; x2++ ) {
-                proposed.add( (double)0.0, (double)1.0, (double)0.0 );
-                // victim.sendMessage("  At " + proposed.getY() );
+            if( rnd.nextBoolean() )
+                z = (rnd.nextDouble() * 200);
+            else
+                z = -(rnd.nextDouble() * 200);
+
+            y = (double)0.0;
+            has_safespot = false;
+
+            proposed = current.clone();
+            proposed.add( x, y, z );
+            proposed.setY( (double)world.getMaxHeight() - 3.0 ); // Set it so we don't get out of sync..
+
+            for( double y2 = proposed.getY(); y2 >= (double)5.0; y2-- ) {
+                proposed.subtract( (double)0.0, (double)1.0, (double)0.0 );
+
                 block = proposed.getBlock();
 
-                if( block.getType() == Material.AIR &&
-                    block.getRelative(BlockFace.DOWN).getType() != Material.LAVA ) {
-                    safe_cnt++;
-                    // System.out.println("Good " + safe_cnt );
-                }
-                else if( block.getType() != Material.AIR && block.getType() != Material.LAVA ) {
-                    // So we don't drop people into bottomless pits
-                    // System.out.println( "found a floor" );
-                    safe_cnt = 0;
-                    has_floor = true;
-                }
-                else {
-                    safe_cnt = 0;
-                    has_floor = false;
+                if( block.getType() == Material.AIR )
+                {
+                    block_under = block.getRelative(BlockFace.DOWN);
+
+                    if( block_under.getType() == Material.AIR ) {
+                        block_under = block_under.getRelative(BlockFace.DOWN);
+
+                        if( block_under.getType() != Material.AIR && block_under.getType() != Material.LAVA ) {
+                            victim.sendMessage("Good we found a spot on solid " + block_under.getType() );
+                            victim.sendMessage("At x:" + proposed.getX() + " y:" + y2 + " z:" + proposed.getZ());
+                            proposed.subtract( (double)0.0, (double)1.0, (double)0.0 ); // So we get in the the tested region.
+                            has_safespot = true;
+                            break;
+                        }
+                    }
                 }
 
-                if( safe_cnt >= 3 )
-                    break;
             }
-
-            // Try to find a floor
-            if ( !has_floor ) {
-                // victim.sendMessage("Looking for floor");
-                safe_cnt = 0;
-                proposed.setY( current.getY() );
-                for( double x2 = proposed.getY(); x2>=4.0; x2-- ) {
-                    proposed.subtract( (double)0.0, (double)1.0, (double)0.0 );
-                    // victim.sendMessage("  At " + proposed.getY() );
-                    block = proposed.getBlock();
-
-                    if( block.getType() == Material.AIR &&
-                        block.getRelative(BlockFace.DOWN).getType() != Material.LAVA ) {
-                        safe_cnt++;
-                        // victim.sendMessage("Good " + safe_cnt );
-                    }
-                    else if( block.getType() != Material.AIR && block.getType() != Material.LAVA && safe_cnt >= 3) {
-                        //So we don't drop people into bottomless pits
-                        safe_cnt++;
-                        has_floor = true;
-                    }
-                    else {
-                        safe_cnt = 0;
-                        has_floor = false;
-                    }
-
-                    if( safe_cnt >= 3 && has_floor ) {
-                        proposed.add((double)0.0, (double)6.0, (double)0.0 ); // hack to avoid the fix below...
-                        break;
-                    }
-                }
-            }
-
         }
 
-        proposed.subtract( (double)0.0, (double)3.0, (double)0.0 );
+        // If needed load the chunk (just to be safe).
+        chunk = proposed.getChunk();
+        if( !chunk.isLoaded() )
+            chunk.load();
+
         victim.teleport( proposed );
         return proposed;
     }
